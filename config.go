34ef2ec492d23d0168189850ddaae2fa06ad84c1
Frank Rast <frank.rast@gmail.com>
2025-03-30 14:51:38 +0000

    refactor config handling to use YAML instead of JSON

--- config.go
+++ config.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"gopkg.in/yaml.v3"
 )
 
 type GlobalConfig struct {
-	Prompts map[string]string `json:"prompts"`
+	Prompts map[string]string `yaml:"prompts"`
 }
 
 func (c *GlobalConfig) LookupPrompt(key string) string {
-	prompt := c.Prompts[key]
-	if prompt == "" {
+	prompt, ok := c.Prompts[key]
+	if !ok {
 		return key
 	}
 	return prompt
@@ -33,32 +33,33 @@ func configDir() string {
 
 func readOrCreateConfig(conf *GlobalConfig) error {
 	dir := configDir()
-	path := filepath.Join(dir, "config.json")
+	path := filepath.Join(dir, "config.yaml")
 
 	f, err := os.Open(path)
-	if errors.Is(err, os.ErrNotExist) {
-		err = os.MkdirAll(filepath.Dir(path), 0o700)
-		if err != nil {
-			return fmt.Errorf("failed to create config dir: %w", err)
-		}
-		f, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0600)
-		if err != nil {
-			return fmt.Errorf("failed to create config file: %w", err)
-		}
-		defer func() { _ = f.Close() }()
-		enc := json.NewEncoder(f)
-		enc.SetIndent("", "  ")
-		err = enc.Encode(conf)
-		if err != nil {
-			return fmt.Errorf("failed to write config file: %w", err)
-		}
-		return nil
-	}
 	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.MkdirAll(filepath.Dir(path), 0o700)
+			if err != nil {
+				return fmt.Errorf("failed to create config dir: %w", err)
+			}
+			f, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0600)
+			if err != nil {
+				return fmt.Errorf("failed to create config file: %w", err)
+			}
+			defer func() { _ = f.Close() }()
+			enc := yaml.NewEncoder(f)
+			enc.SetIndent(2) // Use 2 spaces for indentation
+			err = enc.Encode(conf)
+			if err != nil {
+				return fmt.Errorf("failed to write config file: %w", err)
+			}
+			return nil
+		}
 		return fmt.Errorf("failed to open config file: %w", err)
 	}
 	defer func() { _ = f.Close() }()
-	err = json.NewDecoder(f).Decode(conf)
+	dec := yaml.NewDecoder(f)
+	err = dec.Decode(conf)
 	if err != nil {
 		return fmt.Errorf("failed to read config file: %w", err)
 	}
@@ -68,8 +69,8 @@ func readOrCreateConfig(conf *GlobalConfig) error {
 func InitConfig() (GlobalConfig, error) {
 	conf := GlobalConfig{
 		Prompts: map[string]string{
-			"default": `You are an expert software developer. You will always output every file in TOTAL if you changed it. 
-Do not output unchanged files. 
+			"default": `You are an expert software developer. You will always output every file in TOTAL if you changed it.
+Do not output unchanged files.
 You will ask questions to make sure you understood everything perfectly right.
 Also ask for further info, like documentation, files etc. if needed.
 I provide you with the content of several files. Every file will be introduced by '--- START File: [path] ---' and ended by '--- END FILE ---'
@@ -78,8 +79,8 @@ This is your task:
 
 
 `,
-			"go": `You are an expert go software developer. You will always output every file in TOTAL if you changed it. 
-Do not output unchanged files. 
+			"go": `You are an expert go software developer. You will always output every file in TOTAL if you changed it.
+Do not output unchanged files.
 You will ask questions to make sure you understood everything perfectly right.
 Also ask for further info, like documentation, files etc. if needed.
 I provide you with the content of several files. Every file will be introduced by '--- START File: [path] ---' and ended by '--- END FILE ---'
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.23.0
 
 toolchain go1.24.1
 
-require golang.org/x/sys v0.31.0
+require (
+	golang.org/x/sys v0.31.0
+	gopkg.in/yaml.v3 v3.0.1
+)
