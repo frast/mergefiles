129d02fb4204dc2b1ad83f7d5630a1d8aef6c8b1
Frank Rast <frank.rast@gmail.com>
2025-03-30 23:48:58 +0200

    fix: update mergefiles feature version and installation script URLs

--- feature/mergefiles/install.sh
+++ feature/mergefiles/install.sh
@@ -5,7 +5,7 @@ echo "Activating feature 'mergefiles'"
 
 releaseVersion=${RELEASEVERSION:-undefined}
 
-[ $(uname -m) = x86_64 ] && curl -L https://github.com/frast/mergefiles/releases/download/$releaseVersion/mergefiles_$releaseVersion_linux_amd64.tar.gz | tar xz mergefiles
-[ $(uname -m) = aarch64 ] && curl -L https://github.com/cnoe-io/cnoe-cli/releases/download/$releaseVersion/mergefiles_$releaseVersion_linux_arm64.tar.gz | tar xz mergefiles
+[ $(uname -m) = x86_64 ]  && curl -L "https://github.com/frast/mergefiles/releases/download/${releaseVersion}/mergefiles_${releaseVersion}_linux_amd64.tar.gz" | tar xz mergefiles
+[ $(uname -m) = aarch64 ] && curl -L "https://github.com/frast/mergefiles/releases/download/${releaseVersion}/mergefiles_${releaseVersion}_linux_arm64.tar.gz" | tar xz mergefiles
 chmod +x ./mergefiles
 mv ./mergefiles /usr/local/bin/mergefiles
